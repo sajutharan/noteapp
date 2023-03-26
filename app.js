d71b5d4640f6f0108ceb7dbc1eebe776800b63ea
sajutharan <sajumax08@gmail.com>
2023-03-27 01:10:31 +0530

    main

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
+const Note = require('./models/note');
+const notesRouter = require('./routes/notes');
+const methodOverride = require('method-override');
+require('dotenv').config();
 
-const app = express ();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
-app.get('/', (req, res) => {
-    res.send(`Yayyy! It's working`);
+app.get('/', async (req, res) => {
+  const notes = await Note.find().sort('-createdAt');
+  res.render('index', { notes: notes });
 });
 
+mongoose.connect(process.env.SERVER, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use('/', notesRouter);
 
 // creatting server
 
